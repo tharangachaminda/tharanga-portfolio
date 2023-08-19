cf90889c928d1828a46763b38eb6055663ff169c
Tharanga Nandasena <tharanga@chainx.se>
2023-08-20 02:49:43 +0530

    initial images and icons added. color theme applied

--- app.py
+++ app.py
@@ -4,46 +4,54 @@ app = Flask(__name__)
 
 my_work = [
     {
-        'title': 'SpaceX Falcon 9 1st Stage Landing Prediction - Analysis',
-        'description': '''<ol>
-                            <li>Python statistical libraries and Mathematical libraries for data analysis</li>
-                            <li>SQL language</li>
-                            <li>SQlite</li>
-                            <li><a href="https://python-visualization.github.io/folium/">Folium</a> for interative Maps</li>
-                            <li>Various Python libraries to visualize different insights along the way</li>
-                            <li><a href="https://dash.plotly.com/">Plotly Dash</a> for interactive dashboard</li>
-                            <li>Heroku deployment</li>
-                          </ol>  
-                            ''',
-        'github': 'https://github.com/tharangachaminda/popularity-based-recommendation-system',
-        'icons': ['Python', 'Jupyter-lab', 'SQL', 'SQLight', 'Folium', 'Plotly-dash', 'Heroku']
-    },
-
-    {
+        'header': 'End-to-end',
         'title': 'Black Friday Purchase Prediction',
-        'description': '''<ol>
-                            <li>Exploratory Data Analysis</li>
-                            <li>Pandas</li>
-                            <li>Flask</li>
-                            <li>Heroku deployment</li>
-                          </ol>  
-                            ''',
-        'github': 'https://github.com/tharangachaminda/Black_Friday_Purchase',
-        'icons': ['Python', 'Jupyter-lab', 'SQL', 'SQLight', 'Folium', 'Plotly-dash', 'Heroku']
+        'description': "Banknote analysis refers to the examination of paper currency to determine its legitimacy and identify potential counterfeits. In this python project, I am trying to build a <b>Classification Machine Learning models</b> to predict banknotes are genuine or forged.",
+        'github': 'https://github.com/tharangachaminda/banknotes_analysis',
+        'icons': ['python', 'jupyterlab', 'flask', 'heroku']
     },
 
     {
-        'title': 'Netflix Recommender System (Content Based)',
-        'description': '''<ol>
-                            <li>NLP</li>
-                            <li>NLTK Toolkit</li>
-                            <li>Flask</li>
-                            <li>Heroku deployment</li>
-                          </ol>  
-                            ''',
-        'github': 'https://github.com/tharangachaminda/popularity-based-recommendation-system',
-        'icons': ['Python', 'Jupyter-lab', 'SQL', 'SQLight', 'Folium', 'Plotly-dash', 'Heroku']
+        'header': 'End-to-end',
+        'title': 'Banknotes Authentication',
+        'description': "Netflix Recommender system is one of the best recommender systems in the world. In this project I've used <b>movies datasets</b> and <b>Cosine similarity</b> to build this content based recommender system.",
+        'github': 'https://github.com/tharangachaminda/content_based_recommender_system',
+        'icons': ['python', 'jupyterlab', 'nltk', 'flask', 'heroku']
     },
+
+    {
+        'header': 'Analysis',
+        'title': 'SpaceX Falcon 9 1st Stage Landing Prediction',
+        'description': "SpaceX re-uses the Stage 1 boosters of Falcon 9 rockets. This project is an analysis for successful Stage 1 landing prediction. I have used SpaceX API and Webscraping for data collection, SQlite database for data storage.",
+        'github': 'https://github.com/tharangachaminda/popularity-based-recommendation-system',
+        'icons': ['python', 'jupyterlab', 'sql', 'sqlite', 'folium', 'plotly-dash', 'heroku']
+    },
+
+    {
+        'header': 'Recommender System',
+        'title': 'Netflix Recommender System (Popularity Based)',
+        'description': "Netflix Recommender system is one of the best recommender systems in the world. In this project I've used <b>movies and rating</b> datasets and <b>NLTK toolkit</b> to build this popularity based recommender system.",
+        'github': 'https://github.com/tharangachaminda/popularity-based-recommendation-system',
+        'icons': ['Python', 'jupyterlab', 'flask', 'heroku']
+    },
+
+    {
+        'header': 'Recommender System',
+        'title': 'Netflix Recommender System (Content Based)',
+        'description': "Netflix Recommender system is one of the best recommender systems in the world. In this project I've used <b>movies datasets</b> and <b>Cosine similarity</b> to build this content based recommender system.",
+        'github': 'https://github.com/tharangachaminda/content_based_recommender_system',
+        'icons': ['Python', 'jupyterlabb', 'nltk', 'flask', 'heroku']
+    },
+
+    {
+        'header': 'Analysis',
+        'title': 'Bookstore Web scraping',
+        'description': "In this project I have built a mechanism to collect information about every book in the website, scraping through pagination. This project is associated with https://books.toscrape.com/ website which is specially design for training web scraping. ",
+        'github': 'https://github.com/tharangachaminda/bookstore_webscraping',
+        'icons': ['Python', 'jupyterlab', 'flask', 'heroku']
+    },
+
+    
 ]
 
 @app.route("/")
