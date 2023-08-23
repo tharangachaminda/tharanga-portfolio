e0486be2e55fba99b5ff8f3d80380f5c92641483
Tharanga Nandasena <tharanga@chainx.se>
2023-08-24 00:19:10 +0530

    images added. colors cahanges

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 my_work = [
     {
         'header': 'End-to-end',
+        'image': 'black_friday.png',
         'title': 'Black Friday Purchase Prediction',
         'description': "Banknote analysis refers to the examination of paper currency to determine its legitimacy and identify potential counterfeits. In this python project, I am trying to build a <b>Classification Machine Learning models</b> to predict banknotes are genuine or forged.",
         'github': 'https://github.com/tharangachaminda/banknotes_analysis',
@@ -13,6 +14,7 @@ my_work = [
 
     {
         'header': 'End-to-end',
+        'image': 'bank_notes.png',
         'title': 'Banknotes Authentication',
         'description': "Netflix Recommender system is one of the best recommender systems in the world. In this project I've used <b>movies datasets</b> and <b>Cosine similarity</b> to build this content based recommender system.",
         'github': 'https://github.com/tharangachaminda/content_based_recommender_system',
@@ -21,6 +23,7 @@ my_work = [
 
     {
         'header': 'Analysis',
+        'image': 'falcon_9.png',
         'title': 'SpaceX Falcon 9 1st Stage Landing Prediction',
         'description': "SpaceX re-uses the Stage 1 boosters of Falcon 9 rockets. This project is an analysis for successful Stage 1 landing prediction. I have used SpaceX API and Webscraping for data collection, SQlite database for data storage.",
         'github': 'https://github.com/tharangachaminda/popularity-based-recommendation-system',
@@ -29,6 +32,7 @@ my_work = [
 
     {
         'header': 'Recommender System',
+        'image': 'netflix.png',
         'title': 'Netflix Recommender System (Popularity Based)',
         'description': "Netflix Recommender system is one of the best recommender systems in the world. In this project I've used <b>movies and rating</b> datasets and <b>NLTK toolkit</b> to build this popularity based recommender system.",
         'github': 'https://github.com/tharangachaminda/popularity-based-recommendation-system',
@@ -37,6 +41,7 @@ my_work = [
 
     {
         'header': 'Recommender System',
+        'image': 'netflix.png',
         'title': 'Netflix Recommender System (Content Based)',
         'description': "Netflix Recommender system is one of the best recommender systems in the world. In this project I've used <b>movies datasets</b> and <b>Cosine similarity</b> to build this content based recommender system.",
         'github': 'https://github.com/tharangachaminda/content_based_recommender_system',
@@ -45,6 +50,7 @@ my_work = [
 
     {
         'header': 'Analysis',
+        'image': 'bookstore.png',
         'title': 'Bookstore Web scraping',
         'description': "In this project I have built a mechanism to collect information about every book in the website, scraping through pagination. This project is associated with https://books.toscrape.com/ website which is specially design for training web scraping. ",
         'github': 'https://github.com/tharangachaminda/bookstore_webscraping',
